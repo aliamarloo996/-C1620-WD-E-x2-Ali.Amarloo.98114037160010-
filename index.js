3e0cf2ff75405ce5f2e109d86b0d10f6fd429983
aliamarloo996 <117915970+aliamarloo996@users.noreply.github.com>
2022-11-10 19:01:51 +0330

    var add

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ document.write("visit in sit");
 window.prompt("what u name?");
 console.log("leave scret message");
 var date = Date();
-document.write(date);
+document.write(date +"<br>");
+var res = 5;
+var r = 6;
+document.write(res*r +"<br>");
+var str ="hi ";
+str +="douck";
+document.write(str+"<br>");
+var x = 5;
+var y = 6;
+    document.write(x==y , "<br>" , x!=y , "<br>");
+    var q = 25;
+    document.write((15>x)&&(x<5));
+    
